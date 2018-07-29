cf712d2fbf7956c93b29392ff77646c8c58ed069
Filip Szczepański <jazz2rulez@gmail.com>
2018-07-29 05:22:41 +0200

    "wasd" accelerators for tile selection

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 * Add help screen
 * Update README.md
-* wasd tile selection
 * "file modified" flag and dialogs
 * Implement colour picking
 * Tile, foreground and background toggles
--- src/ui/components/tile_chooser.rs
+++ src/ui/components/tile_chooser.rs
@@ -1,9 +1,13 @@
+use std::cmp::min;
 use std::rc::Rc;
 
 use gtk;
 use gtk::prelude::*;
 
 use gdk::{EventButton, EventMask};
+
+use gio::prelude::*;
+use gio::SimpleAction;
 
 use ui::canvas::Tile;
 use ui::tileset;
@@ -58,8 +62,105 @@ pub fn build(state: &Rc<State>) -> gtk::DrawingArea {
         move |area, event| pressed(area, &state, event)
     });
 
+    add_actions(state, &area);
+    add_accelerators(state);
+
     area.show_all();
     area
+}
+
+fn add_actions(state: &Rc<State>, area: &gtk::DrawingArea) {
+    let up = SimpleAction::new("tile.up", None);
+    up.connect_activate({
+        let state = state.clone();
+        let area = area.downgrade();
+        move |_, _| {
+            {
+                let mut tile = state.current_tile.borrow_mut();
+                let index = tile.index;
+                let (x, mut y) = (index & 0xF, index >> 4);
+                y = y.saturating_sub(1);
+                tile.index = x | y << 4;
+            }
+            match area.upgrade() {
+                Some(area) => {
+                    area.queue_draw();
+                }
+                None => return,
+            }
+        }
+    });
+    let down = SimpleAction::new("tile.down", None);
+    down.connect_activate({
+        let state = state.clone();
+        let area = area.downgrade();
+        move |_, _| {
+            {
+                let mut tile = state.current_tile.borrow_mut();
+                let index = tile.index;
+                let (x, mut y) = (index & 0xF, index >> 4);
+                y = min(y + 1, 15);
+                tile.index = x | y << 4;
+            }
+            match area.upgrade() {
+                Some(area) => {
+                    area.queue_draw();
+                }
+                None => return,
+            }
+        }
+    });
+    let left = SimpleAction::new("tile.left", None);
+    left.connect_activate({
+        let state = state.clone();
+        let area = area.downgrade();
+        move |_, _| {
+            {
+                let mut tile = state.current_tile.borrow_mut();
+                let index = tile.index;
+                let (mut x, y) = (index & 0xF, index >> 4);
+                x = x.saturating_sub(1);
+                tile.index = x | y << 4;
+            }
+            match area.upgrade() {
+                Some(area) => {
+                    area.queue_draw();
+                }
+                None => return,
+            }
+        }
+    });
+    let right = SimpleAction::new("tile.right", None);
+    right.connect_activate({
+        let state = state.clone();
+        let area = area.downgrade();
+        move |_, _| {
+            {
+                let mut tile = state.current_tile.borrow_mut();
+                let index = tile.index;
+                let (mut x, y) = (index & 0xF, index >> 4);
+                x = min(x + 1, 15);
+                tile.index = x | y << 4;
+            }
+            match area.upgrade() {
+                Some(area) => {
+                    area.queue_draw();
+                }
+                None => return,
+            }
+        }
+    });
+    state.app.add_action(&up);
+    state.app.add_action(&down);
+    state.app.add_action(&left);
+    state.app.add_action(&right);
+}
+
+fn add_accelerators(state: &Rc<State>) {
+    state.app.set_accels_for_action("app.tile.up", &["w"]);
+    state.app.set_accels_for_action("app.tile.down", &["s"]);
+    state.app.set_accels_for_action("app.tile.left", &["a"]);
+    state.app.set_accels_for_action("app.tile.right", &["d"]);
 }
 
 fn pressed(area: &gtk::DrawingArea, state: &Rc<State>, event: &EventButton) -> Inhibit {
